ae131d24c1b83a6971d6485f4be2933e184696b9
chaoxiaoshu-mx <leukotrichia@163.com>
2020-07-06 15:17:08 +0800

    增加分类迁移

---
+++ database/migrations/2020_07_06_065816_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('parent_id');   // 父级分类id
+            $table->integer('level');       // 分类等级
+            $table->string('icon');
+            $table->timestamps();
+            $table->softDeletes();  // 软删除
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,12 @@
+!<!DOCTYPE html>
+<html>
+<head>
+	<script type="text/javascript" src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
+<script type="text/javascript" src="star_rtc_video.min.js"></script>
+<script type="text/javascript" src="star_rtc_lib.min.js"></script>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
